5ffa8dad8b00eb41060b5060b49fdf8896198915
haoyu234 <164346864@qq.com>
2025-04-04 14:27:58 +0800

    使用 borrow: `.` 简化代码

--- src/instru/queue.nim
+++ src/instru/queue.nim
@@ -1,54 +1,54 @@
 type
   InstruQueueNode* = object
-    previous: ptr InstruQueueNode
+    prev: ptr InstruQueueNode
     next: ptr InstruQueueNode
 
-  InstruQueue* = distinct InstruQueueNode
+  InstruQueue* {.borrow: `.`.} = distinct InstruQueueNode
+
+template head*(h: InstruQueue): ptr InstruQueueNode =
+  h.next
 
 template next*(h: InstruQueueNode): ptr InstruQueueNode =
   h.next
 
-template previous*(h: InstruQueueNode): ptr InstruQueueNode =
-  h.previous
-
-template head*(h: InstruQueue): ptr InstruQueueNode =
-  InstruQueueNode(h).next
-
-template isEmpty*(h: var InstruQueueNode): bool =
-  h.next.isNil or h.addr == h.next
+template prev*(h: InstruQueueNode): ptr InstruQueueNode =
+  h.prev
 
 template isEmpty*(h: var InstruQueue): bool =
   InstruQueueNode(h).isEmpty()
 
+template isEmpty*(h: var InstruQueueNode): bool =
+  h.next.isNil or h.addr == h.next
+
 proc initEmpty*(h: var InstruQueue) {.inline.} =
-  InstruQueueNode(h).next = InstruQueueNode(h).addr
-  InstruQueueNode(h).previous = InstruQueueNode(h).addr
+  h.next = InstruQueueNode(h).addr
+  h.prev = InstruQueueNode(h).addr
 
 proc initEmpty*(h: var InstruQueueNode) {.inline.} =
   h.next = h.addr
-  h.previous = h.addr
+  h.prev = h.addr
 
 proc insertFront*(h: var InstruQueue, n: var InstruQueueNode) {.inline.} =
   assert n.isEmpty
 
-  n.next = InstruQueueNode(h).next
-  n.previous = InstruQueueNode(h).addr
-  n.next.previous = n.addr
-  InstruQueueNode(h).next = n.addr
+  n.next = h.next
+  n.prev = InstruQueueNode(h).addr
+  n.next.prev = n.addr
+  h.next = n.addr
 
 proc insertBack*(h: var InstruQueue, n: var InstruQueueNode) {.inline.} =
   assert n.isEmpty
 
   n.next = InstruQueueNode(h).addr
-  n.previous = InstruQueueNode(h).previous
-  n.previous.next = n.addr
-  InstruQueueNode(h).previous = n.addr
+  n.prev = h.prev
+  n.prev.next = n.addr
+  h.prev = n.addr
 
 proc mergeInto*(h, n: var InstruQueue) {.inline.} =
-  InstruQueueNode(n).previous.next = InstruQueueNode(h).next
-  InstruQueueNode(h).next.previous = InstruQueueNode(n).previous
-  InstruQueueNode(n).previous = InstruQueueNode(h).previous
-  InstruQueueNode(n).previous.next = InstruQueueNode(n).addr
+  n.prev.next = h.next
+  h.next.prev = n.prev
+  n.prev = h.prev
+  n.prev.next = InstruQueueNode(n).addr
 
   initEmpty(h)
 
@@ -56,33 +56,33 @@ proc moveInto*(h, n: var InstruQueue) {.inline.} =
   if h.isEmpty():
     n.initEmpty()
   else:
-    let q = InstruQueueNode(h).next
-    InstruQueueNode(n).previous = InstruQueueNode(h).previous
-    InstruQueueNode(n).previous.next = InstruQueueNode(n).addr
+    let q = h.next
+    n.prev = h.prev
+    n.prev.next = InstruQueueNode(n).addr
     InstruQueueNode(n).next = q
-    InstruQueueNode(h).previous = q.previous
-    InstruQueueNode(h).previous.next = InstruQueueNode(h).addr
-    q.previous = InstruQueueNode(n).addr
+    h.prev = q.prev
+    h.prev.next = InstruQueueNode(h).addr
+    q.prev = InstruQueueNode(n).addr
 
 proc remove*(h: var InstruQueueNode) {.inline.} =
   assert not h.isEmpty
 
-  h.previous.next = h.next
-  h.next.previous = h.previous
+  h.prev.next = h.next
+  h.next.prev = h.prev
   h.initEmpty
 
 proc popFront*(h: var InstruQueue): ptr InstruQueueNode {.inline.} =
-  let n = InstruQueueNode(h).next
+  let n = h.next
   remove(n[])
   n
 
 proc popBack*(h: var InstruQueue): ptr InstruQueueNode {.inline.} =
-  let n = InstruQueueNode(h).previous
+  let n = h.prev
   remove(n[])
   n
 
 iterator items*(h: var InstruQueue): ptr InstruQueueNode =
-  var i = InstruQueueNode(h).next
+  var i = h.next
   let q = InstruQueueNode(h).addr
 
   while i != q:
@@ -91,8 +91,8 @@ iterator items*(h: var InstruQueue): ptr InstruQueueNode =
     i = n
 
 iterator items*(h: InstruQueue): ptr InstruQueueNode =
-  var i = InstruQueueNode(h).next
-  let q = i.previous
+  var i = h.next
+  let q = i.prev
 
   while i != q:
     let n = i.next
